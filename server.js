c1756df571eeceb51da30ae5553f88add8c60f8a
Robert Lombardo <robert@wheelwell.com>
2019-02-03 06:39:16 +0900

    Initial Commit - ported chat_server from dungeon-team

---
+++ server.js
@@ -0,0 +1,28 @@
+const express    = require('express')
+const bodyParser = require('body-parser')
+const sockets    = require('./sockets')
+
+const app = express()
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');    
+    res.header('Access-Control-Expose-Headers', '*');
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.header( 'Upgrade', '$http_upgrade' );
+    res.header( 'Connection', "upgrade" );
+
+    if( req.method === 'OPTIONS' ) {
+        res.sendStatus(200);
+    } else {
+        return next();
+    }
+})
+
+sockets.init(app)
